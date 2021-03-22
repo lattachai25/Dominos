2d18826f4291153684c78379be8f203bbf50d17e
lattachai25 <lattachai25@gmail.com>
2021-03-22 16:10:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,86 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-import Orders from './theseason_98/Orders'
-import Details_Coupons_report from './theseason_98/Details_Coupons_report'
-import ProductsText from './theseason_98/Details_Coupons_report'
-
-
+import Sukhumvit22 from "./sukhumvit_22/theseason";
+import Sukhumvit01 from "./sukhumvit_01/theseason";
+import Sukhumvit103 from "./sukhumvit_103/theseason";
+import Pattaya from "./pattaya/theseason";
+import Samyan from "./samyan/theseason";
+import PinKlao from "./pinKlao/theseason";
+import Plearnary_Mall from "./plearnary_mall/theseason";
+import St_Louis from "./st_louis/theseason";
+import Victoria_Garden from "./victoria_garden/theseason";
+import TheNine from "./the_nine/theseason";
+import Senafest from "./sena_fest/theseason";
+import Muang_Thong_Thani from "./muang_thong_thani/theseason";
+import Suthiisan from "./suthiisan/theseason";
+import Amorini from "./amorini/theseason";
+import Metropole_Thonglor from "./metropole_thonglor/theseason";
+import Seacon_BangKae from "./seacon_bangKae/theseason";
+import Silom from "./silom/theseason";
+import Jas_Sinakarin from "./jas_sinakarin/theseason";
+import Kaset from "./kaset/theseason";
+// import Nichida_Thani from "./nichida_thani/theseason";
+import China_Town from "./china_town/theseason";
+import Bangkapi from "./bangkapi/theseason";
+import BangluamPoo from "./bangluam_poo/theseason";
+import RatnaKosin from "./ratnakosin/theseason";
+import Ladkrabang from "./ladkrabang/theseason";
+import Nonthaburi from "./nonthaburi/theseason";
+import TheSeason from "./theseason_98/theseason";
 
 function App() {
   return (
-    <div>
-<div className="row">
+    <div className="container">
+      <br />
+      <br />
+      <div className="alert alert-primary" role="alert">
+        Dominos Pizza
+      </div>
 
-<div className="card" style={{width: '18rem'}}>
-  <h2>The Season</h2>
-  <div className="card-body">
-        <Orders />
-        <ProductsText />
-        <Details_Coupons_report />
-  </div>
-</div>
+      <div className="row">
+        <Sukhumvit22 />
+        <Sukhumvit01 />
+        <Sukhumvit103 />
 
+        <Pattaya />
+        <Samyan />
+        <PinKlao />
 
-<div className="card" style={{width: '18rem'}}>
-  <h2>Orders</h2>
-  <div className="card-body">
-        <Orders />
-        <ProductsText />
-        <Details_Coupons_report />
-  </div>
-</div>
+        <Plearnary_Mall />
+        <St_Louis />
+        <Victoria_Garden />
 
+        <TheNine />
+        <Senafest />
+        <Muang_Thong_Thani />
 
-</div>
+        <Suthiisan />
+        <Amorini />
+        <Metropole_Thonglor />
+
+        <Seacon_BangKae />
+        <Silom />
+        <Jas_Sinakarin />
+
+        <Kaset />
+        {/* <Nichida_Thani /> */}
+        <China_Town />
+
+        <Bangkapi />
+        <BangluamPoo />
+        <RatnaKosin />
+
+        <Ladkrabang />
+        <Nonthaburi />
+        <TheSeason />
+      </div>
+
+      <div class="card-header text-center">
+        Copyright@ Dominos Pizza 2021 - 2030
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/victoria_garden/theseason.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Orders from "./Orders";
+import Details_Coupons_report from "./Details_Coupons_report";
+import ProductsText from "./ProductsText";
+
+function theseason() {
+  return (
+    <div>
+      <div className="col-3">
+        <div className="card" style={{ width: "21.5rem" }}>
+          {/* <img src="..." className="card-img-top" alt="..." /> */}
+          <div className="card-body">
+            <h5 className="card-title">Victoria Garden</h5>
+            <p className="card-text">The Report By Branch.</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <Orders />
+            </li>
+            <li className="list-group-item">
+              <ProductsText />
+            </li>
+            <li className="list-group-item">
+              <Details_Coupons_report />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default theseason;
